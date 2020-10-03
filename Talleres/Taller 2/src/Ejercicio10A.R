e183750a67efd5924e99317b733166f21aaeaedd
Ricardo Bernal Alfonso <RicardoBernalAlfonso@users.noreply.github.com>
2020-10-03 01:03:40 -0500

    Taller 2 - Ejercicio 8 completado

--- Talleres/Taller 2/src/Ejercicio10A.R
+++ Talleres/Taller 2/src/Ejercicio10A.R
@@ -1,7 +1,6 @@
 #Ejercicio 10 - Parte A
 #NOTA: Se debe instalar lib BB
 
-library("BB")
 require(BB)
 
 sistema = function(x){
@@ -12,14 +11,14 @@ sistema = function(x){
 }
 
 p0 = c(1,1)
-sol1=BBsolve(par=p0, fn=sistema)
+sol1=BBsolve(par=p0, fn=sistema) # msg: Succesful convergence
 cat("Solucion inicial de (1,1): \n",sol1[[1]],"\n")
 p0 = c(-1,1)
-sol2=BBsolve(par=p0, fn=sistema)
+sol2=BBsolve(par=p0, fn=sistema) # msg: Succesful convergence
 cat("Solucion inicial de (-1,1): \n",sol2[[1]],"\n")
 
 # Las soluciones para x^2+y^2=1 son:
-# y1 = sqrt(1-x^2); y2 = sqrt(1-x^2)
+# y1 = sqrt(1-x^2); y2 = sqrt(1-x^2) -- Con Centro en (0,0)
 
 f1=function(x){
   sqrt(1-x^2)
@@ -38,8 +37,8 @@ f3=function(x){
 
 x=seq(-1,1,0.00001) # Con una tolerancia de 10e-6
 plot(x,f3(x),type="l",col="red")
-abline(h=0,col="blue")
-lines(x,f1(x),type="l",col="green")
-lines(x,f2(x),type="l",col="green")
-points(rbind(c(sqrt(2)/2,sqrt(2)/2)),pch=17,cex=1.5,col="red")
-points(rbind(c(-sqrt(2)/2,-sqrt(2)/2)),pch=17,cex=1.5,col="red")
+abline(h=0,col="blue") # Eje X
+lines(x,f1(x),type="l",col="green") # Circunferencia arriba del eje X
+lines(x,f2(x),type="l",col="green") # Circunferencia por debajo del eje X
+points(rbind(c(sqrt(2)/2,sqrt(2)/2)),pch=17,cex=1.5,col="red") # Puntos de Interseccion
+points(rbind(c(-sqrt(2)/2,-sqrt(2)/2)),pch=17,cex=1.5,col="red") # Puntos de Interseccion
